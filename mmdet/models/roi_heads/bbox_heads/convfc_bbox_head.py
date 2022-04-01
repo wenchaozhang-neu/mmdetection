3adc1ccc95b36f68294da404c11177ab6e716acb
wenchaozhang-neu <90308986+wenchaozhang-neu@users.noreply.github.com>
2022-04-02 04:51:35 +0800

    Update convfc_bbox_head.py

--- mmdet/models/roi_heads/bbox_heads/convfc_bbox_head.py
+++ mmdet/models/roi_heads/bbox_heads/convfc_bbox_head.py
@@ -1,11 +1,135 @@
-# Copyright (c) OpenMMLab. All rights reserved.
 import torch.nn as nn
 from mmcv.cnn import ConvModule
-
+import torch
 from mmdet.models.builder import HEADS
 from mmdet.models.utils import build_linear_layer
 from .bbox_head import BBoxHead
+class Linear_Eca_block(nn.Module):
+    """docstring for Eca_block"""
+    def __init__(self):
+        super(Linear_Eca_block, self).__init__()
+        self.avgpool = nn.AdaptiveAvgPool2d(1)
+        self.conv1d = nn.Conv1d(1, 1, kernel_size=5, padding=int(5/2), bias=False)
+        self.sigmoid = nn.Sigmoid()
+    def forward(self, x, gamma=2, b=1):
+        #N, C, H, W = x.size()
+        y = self.avgpool(x)
+        y = self.conv1d(y.squeeze(-1).transpose(-1, -2))
+        y = y.transpose(-1, -2).unsqueeze(-1)
+        y = self.sigmoid(y)
 
+        return y.expand_as(x)
+
+class Linear_Eca_block_reduce(nn.Module):
+    """docstring for Eca_block"""
+    def __init__(self):
+        super(Linear_Eca_block_reduce, self).__init__()
+        self.conv1d = nn.Conv1d(256, 256, kernel_size=5, stride=2, padding=int(5/2), bias=False)
+        self.relu = nn.ReLU()
+    def forward(self, x):
+        y = torch.flatten(x, start_dim=2)
+        y = self.conv1d(y)
+        y = self.relu(y)
+        return y
+
+class Eca_block(nn.Module):
+    """docstring for Eca_block"""
+    def __init__(self):
+        super(Eca_block, self).__init__()
+        self.avgpool = nn.AdaptiveAvgPool2d(1)
+        self.conv1d = nn.Conv1d(1, 1, kernel_size=3, padding=int(3/2), bias=False)
+        self.sigmoid = nn.Sigmoid()
+    def forward(self, x, gamma=2, b=1):
+        #N, C, H, W = x.size()
+        y = self.avgpool(x)
+        y = self.conv1d(y.squeeze(-1).transpose(-1, -2))
+        y = y.transpose(-1, -2).unsqueeze(-1)
+        y = self.sigmoid(y)
+
+
+        return x * y.expand_as(x)
+
+
+class Relation_self(nn.Module):
+
+    def __init__(self,
+                 in_channels=1,
+                 out_channels=16,
+                 conv_cfg=None,
+                 norm_cfg=dict(type='BN')):
+        super(Relation_self, self).__init__()
+
+        # main path
+        self.conv1 = ConvModule(
+            1,
+            16,
+            kernel_size=1,
+            bias=False,
+            conv_cfg=conv_cfg,
+            norm_cfg=norm_cfg)
+        #use depth-wise conv
+        self.conv2 = ConvModule(
+            16,
+            1,
+            kernel_size=1,
+            bias=False,
+            conv_cfg=conv_cfg,
+            norm_cfg=norm_cfg,
+            act_cfg=None)
+
+        self.relu = nn.ReLU(inplace=True)
+        #self.softmax = nn.Softmax(dim=2)
+        #self.eca = Eca_block()
+
+    def forward(self, x):
+        #x = torch.t(x)
+        x_re = x.view(x.size(0), -1, 32, 32)
+        x_re = self.conv1(x_re)
+        #x_re = self.eca(x_re)
+        x_re = self.conv2(x_re)
+        x_re = x_re.view(x.size(0), -1)
+        x = x + x_re
+        return x
+class Relation_crosss(nn.Module):
+
+    def __init__(self,
+                 in_channels=1,
+                 out_channels=16,
+                 conv_cfg=None,
+                 norm_cfg=dict(type='BN')):
+        super(Relation_crosss, self).__init__()
+
+        # main path
+        self.conv1 = ConvModule(
+            1,
+            16,
+            kernel_size=1,
+            bias=False,
+            conv_cfg=conv_cfg,
+            norm_cfg=norm_cfg)
+        #use depth-wise conv
+        self.conv2 = ConvModule(
+            16,
+            1,
+            kernel_size=1,
+            bias=False,
+            conv_cfg=conv_cfg,
+            norm_cfg=norm_cfg,
+            act_cfg=None)
+
+        self.relu = nn.ReLU(inplace=True)
+        #self.softmax = nn.Softmax(dim=2)
+        #self.eca = Eca_block()
+
+    def forward(self, x):
+        x = torch.t(x)
+        x_re = x.view(x.size(0), -1, 32, 32)
+        x_re = self.conv1(x_re)
+        #x_re = self.eca(x_re)
+        x_re = self.conv2(x_re)
+        x_re = x_re.view(x.size(0), -1)
+        x = torch.t(x + x_re)
+        return x
 
 @HEADS.register_module()
 class ConvFCBBoxHead(BBoxHead):
@@ -77,6 +201,11 @@ class ConvFCBBoxHead(BBoxHead):
             if self.num_reg_fcs == 0:
                 self.reg_last_dim *= self.roi_feat_area
 
+        self.rel_self_1 = Relation_self(in_channels=1, out_channels=16)
+        self.rel_cross_1 = Relation_self(in_channels=1, out_channels=16)
+        self.linear_eca_block_reduce = Linear_Eca_block_reduce()
+        self.gce_p =Linear_Eca_block()
+        self.gpa = nn.AdaptiveAvgPool2d(1)
         self.relu = nn.ReLU(inplace=True)
         # reconstruct fc_cls and fc_reg since input channels are changed
         if self.with_cls:
@@ -97,17 +226,10 @@ class ConvFCBBoxHead(BBoxHead):
                 out_features=out_dim_reg)
 
         if init_cfg is None:
-            # when init_cfg is None,
-            # It has been set to
-            # [[dict(type='Normal', std=0.01, override=dict(name='fc_cls'))],
-            #  [dict(type='Normal', std=0.001, override=dict(name='fc_reg'))]
-            # after `super(ConvFCBBoxHead, self).__init__()`
-            # we only need to append additional configuration
-            # for `shared_fcs`, `cls_fcs` and `reg_fcs`
             self.init_cfg += [
                 dict(
                     type='Xavier',
-                    distribution='uniform',
+                    layer='Linear',
                     override=[
                         dict(name='shared_fcs'),
                         dict(name='cls_fcs'),
@@ -147,7 +269,8 @@ class ConvFCBBoxHead(BBoxHead):
             # for separated branches, also consider self.num_shared_fcs
             if (is_shared
                     or self.num_shared_fcs == 0) and not self.with_avg_pool:
-                last_layer_dim *= self.roi_feat_area
+                #last_layer_dim *= self.roi_feat_area
+                 last_layer_dim *= 25
             for i in range(num_branch_fcs):
                 fc_in_channels = (
                     last_layer_dim if i == 0 else self.fc_out_channels)
@@ -156,7 +279,26 @@ class ConvFCBBoxHead(BBoxHead):
             last_layer_dim = self.fc_out_channels
         return branch_convs, branch_fcs, last_layer_dim
 
-    def forward(self, x):
+    def forward(self, x, p):
+        p2,p3,p4,p5, _ = p
+        from tools.feature_visualization import draw_feature_map
+        draw_feature_map(p)
+        p2 = self.gpa(p2)
+        p3 = self.gpa(p3)
+        p4 = self.gpa(p4)
+        p5 = self.gpa(p5)
+        p = p2 + p3 + p4 + p5
+       
+        gc = self.gce_p(p)
+
+        if gc.shape[0] == 2:
+            x_b1,x_b2 = x.split(512, )
+            gc_b1,gc_b2 = gc.split(1, 0)
+            x_b1 = x_b1 * gc_b1
+            x_b2 = x_b2 * gc_b2
+            x = torch.cat((x_b1, x_b2), 0)
+        else:
+            x = x * gc
         # shared part
         if self.num_shared_convs > 0:
             for conv in self.shared_convs:
@@ -165,11 +307,14 @@ class ConvFCBBoxHead(BBoxHead):
         if self.num_shared_fcs > 0:
             if self.with_avg_pool:
                 x = self.avg_pool(x)
-
+            x = self.linear_eca_block_reduce(x)
             x = x.flatten(1)
+            fc1, fc2 = self.shared_fcs
+            x = self.relu(fc1(x))
+            x = self.rel_self_1(x)
 
-            for fc in self.shared_fcs:
-                x = self.relu(fc(x))
+            x = self.relu(fc2(x))
+            x = self.rel_cross_1(x)
         # separate branches
         x_cls = x
         x_reg = x
